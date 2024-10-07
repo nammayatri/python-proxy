1b0a96fe58c2099a3c0c4e71c4f2a08c42febd14
piyushKumar-1 <pkkapoor98@gmail.com>
2024-10-08 03:21:18 +0530

    added redis-push support

--- push-to-kafka.py
+++ push-to-kafka.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import time
+from rediscluster import RedisCluster
 from confluent_kafka import Producer
 
 # Read configurations from environment variables
@@ -9,11 +10,20 @@ KAFKA_SERVER = os.getenv('KAFKA_SERVER', 'localhost:9092')
 ENDPOINT_URL = os.getenv('ENDPOINT_URL', 'https://example.com/api/vehicle_data')
 INTERVAL = int(os.getenv('INTERVAL', '10'))
 
+REDIS_NODES = os.getenv('REDIS_NODES', 'localhost:7000').split(',')
+
+startup_nodes = [{"host": node.split(":")[0], "port": node.split(":")[1]} for node in REDIS_NODES]
+
+
 # Setup Kafka producer
 producer_config = {
     'bootstrap.servers': KAFKA_SERVER
 }
+
 producer = Producer(producer_config)
+
+redis_client = RedisCluster(startup_nodes=startup_nodes, decode_responses=True, skip_full_coverage_check=True)
+
 
 # Function to deliver messages to Kafka
 def delivery_report(err, msg):
@@ -36,8 +46,14 @@ def scrape_and_send_to_kafka():
                 if 'entity' in data:
                     for entity in data['entity']:
                         # Send each entity as a message to Kafka
-                        producer.produce(KAFKA_TOPIC, key=str(entity['vehicleid']), value=str(entity), callback=delivery_report)
-                        producer.flush()  # Ensure the message is sent immediately
+                        route_id = entity.get('routeId', None)
+                        if route_id != None:
+                            producer.produce(KAFKA_TOPIC, key=str(entity['vehicleid']), value=str(entity), callback=delivery_report)
+                            vehicle_id = str(entity['vehicleid'])
+                            entity_data = {k: str(v) for k, v in entity.items() if k not in ('vehicleid', 'routeId')}
+                            redis_client.hset(f"route:{route_id}", mapping={vehicle_id: str(entity_data)})
+                        else:
+                            print("got null routeId")
                 else:
                     print(f"No 'entity' found in the response: {data}")
             else:
@@ -47,6 +63,7 @@ def scrape_and_send_to_kafka():
             print(f"An error occurred: {e}")
 
         # Wait for the specified interval before scraping again
+        producer.flush()
         time.sleep(INTERVAL)
 
 # Run the scraper
