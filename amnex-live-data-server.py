d1688667e1fad0a5b3bf7dc9953c6430a502b673
Piyush <54761970+piyushKumar-1@users.noreply.github.com>
2025-06-26 20:25:49 +0530

    fix

--- amnex-live-data-server.py
+++ amnex-live-data-server.py
@@ -969,7 +969,7 @@ def parse_mqtt_payload(data_str, serverTime, client_ip):
 
 def parse_payload(data_decoded, client_ip, serverTime, isNYGpsDevice):
     """Parse payload data by determining the format"""
-    print(payload, serverTime, client_ip)
+    print(data_decoded, serverTime, client_ip)
     try:
         # First check if it's NY GPS device mqtt server data
         if isNYGpsDevice:
