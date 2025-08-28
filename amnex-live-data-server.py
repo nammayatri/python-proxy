6df230e88888a985369c2e2f7897d4b56c4ef7c4
Piyush <54761970+piyushKumar-1@users.noreply.github.com>
2025-08-28 17:00:17 +0530

    Update amnex-live-data-server.py

--- amnex-live-data-server.py
+++ amnex-live-data-server.py
@@ -2088,6 +2088,7 @@ def handle_connection(conn, addr):
                 decoded_data_list = data_decoded.split('\n')
                 print("decoded_data_list", len(decoded_data_list), decoded_data_list)
                 for data in decoded_data_list:
+                    print("data", data)
                     # executor.submit(handle_client_data, data, addr, serverTime)
                 # Reset the timeout after each successful read
                 conn.settimeout(300)
